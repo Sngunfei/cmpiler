cf92e5ea7127dd90f3124a38ab7192b6003373be
syf <syfnico@foxmail.com>
2018-04-13 22:58:35 +0800

    预处理差不多完成了

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,20 @@
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
+
 public class Main {
 
     public static void main(String[] args) {
-        long[] N = new long[]{10, 100, 1000};
-        long[] U = new long[]{500000, 1000000, 2000000};
-        final long F = 15*1000*1000*1000;
-        final long us = 30*1000*1000;
-        final long di = 2*1000*1000;
-        for(int i=0; i<3; i++){
-            long ans = Math.max(N[i]*F/us,F/di);
-            System.out.println(ans);
+        try{
+            File file = new File("F:\\output.txt");
+            PrintWriter pw = new PrintWriter(file);
+            pw.println("~~~~~~~~~~~~~~~~~~~");
+            pw.flush();
+            pw.close();
+        }catch (IOException e){
+            e.printStackTrace();
         }
-        System.out.println("***************");
-        for(int i=0; i<3; i++){
-            System.out.println("N=" + N[i]);
-            for(int j=0; j<3; j++) {
-                long ans = Math.max(F/us, Math.max(F/di, N[i]*F/(us + N[i]*U[j])));
-                System.out.println(ans);
-            }
 
-        }
+
     }
 }
--- src/com/syf/Parse.java
+++ src/com/syf/Parse.java
@@ -1,18 +1,28 @@
 package com.syf;
 
-
+import java.io.*;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
 
 class Production{
     // 产生式左部，右部
-    public String left;
-    public String[] right;
+    private String left;
+    private String[] right;
 
     public Production(String s){
         String[] strings = s.split(" ");
         left = strings[0];
         right = new String[strings.length - 1];
         System.arraycopy(strings, 1, this.right,0, strings.length - 1);
+    }
+
+    public String getLeft() {
+        return left;
+    }
+
+    public String[] getRight() {
+        return right;
     }
 }
 
@@ -52,80 +62,189 @@ class SetOfItems{
 
 }
 
-interface Contains{
-    boolean contains();
-}
-
-class terminals implements Contains{
-    public
-}
-
 public class Parse {
 
-    private final int MAX_PRODCUCTION = 1000;
-    private final int MAX_LENGTH = 1000;
+    private final int MAX_PRODCUCTION = 500;
+    private final int MAX_LENGTH = 500;
 
-    private Production[] productions = new Production[MAX_PRODCUCTION];
-    private SymbolTable table = new SymbolTable(null);
+    private ArrayList<Production> productions = new ArrayList<>(MAX_PRODCUCTION);
+    private ArrayList<String> terminals = new ArrayList<>();
+    private ArrayList<String> non_terminals = new ArrayList<>();
 
+    private HashMap<String, ArrayList<Production>> classified_production = new HashMap<>();
+
+    private HashMap<String, ArrayList<String>> firstSet = new HashMap<>();
 
     /*
-     *   归约动作，这时候返回产生式的长度，然后栈中弹出这一串，并把产生式的左部分压栈
+     * read all productions from "Production_table.txt"
+     * store production and identify terminals and non_terminals
      */
-
-    private SetOfItems CLOSURE(SetOfItems I){
-        int size = 0;
-        while(true){
-            for(Item item: I.items) {
-                //String
-                for (Production production : productions) {
-                    if (production.left.equals("B")){
-                        int length = item.right.length - item.dot + 1;
-                        // dot_right里存放着 [A -> α.Bβ, b] 中的 βb 部分
-                        String[] dot_right = new String[length];
-                        System.arraycopy(item.right, item.dot, dot_right, 0, length);
-                        for (String b : getFirst(dot_right)) {
-                            Item newItem = new Item(production, b);
-                            I.insert(newItem);
-                        }
+    private void readIn() throws IOException {
+        File file = new File("C:\\Users\\86234\\workspace\\FirstProj\\src\\com\\syf\\Production_table");
+        BufferedReader in = new BufferedReader(
+                                new InputStreamReader(
+                                        new FileInputStream(file), "UTF-8"));
+        String line;
+        while((line = in.readLine()) != null){
+            this.productions.add(new Production(line));
+            String[] production = line.split(" ");
+            for(int i=0; i<production.length; i++){
+                if(Character.isLetter(production[i].charAt(0)) && Character.isLowerCase(production[i].charAt(0))){
+                    if(!this.non_terminals.contains(production[i])) {
+                        this.non_terminals.add(production[i]);
+                        System.out.println(production[i]);
+                    }
+                }else{
+                    if(!this.terminals.contains(production[i])) {
+                        this.terminals.add(production[i]);
+                        System.out.println(production[i]);
                     }
                 }
             }
-            // until 不能向I中加入更多的项
-            if(I.size > size){
-                size = I.size;
-            }else
-                return I;
         }
     }
 
     /*
-     *   getFirst(x), 求符号x的first集
-     *   如果x是终结符，那么x就是它本身的终结符
-     *   如果x不是终结符，那么x->x1x2x3x4x5, 返回x1的first集，如果x1->ε存在，那么返回x2的first集
-     *   依次类推，如果所有的xi都可以推出空串，那么x的first集中包括ε
+     * classify productions by non-terminals
      */
-    private ArrayList<String> getFirst(String[] X){
-        int length = X.length;
-        ArrayList<String> firstSet = new ArrayList<>(500);
-
-        for(int i = 0; i < length; i++){
-            if(X[i] instanceof terminal){
-                firstSet.add(X[i]);
-                return firstSet;
-            }else if(X[i] instanceof nonTerminal){
-                // 应该找到形如 X[i]->sadasd 的所有产生式
-                Production[] productions = new Production[500];
-                for(Production production: productions){
-                    firstSet.addAll(getFirst(production.right));
+    private void classify_production(){
+        boolean[] visited = new boolean[productions.size()];
+        for(int i=0; i<non_terminals.size(); i++){
+            ArrayList<Production> arrayList = new ArrayList<>(50);
+            for(int j=0; j<this.productions.size(); j++){
+                if(visited[j])
+                    continue;
+                Production production = productions.get(j);
+                if(production.getLeft().equals(this.non_terminals.get(i))){
+                    arrayList.add(production);
+                    visited[j] = true;
                 }
             }
+            classified_production.put(this.non_terminals.get(i), arrayList);
         }
-
     }
 
-    private void getFollow(){
+    /*
+     * 搜索某个非终结符的所有产生式
+     */
+    private ArrayList<Production> getProductionBySymbol(String non_terminal){
+        if(!classified_production.containsKey(non_terminal)){
+            System.out.println("该非终结符不存在！");
+            return null;
+        }
+        return classified_production.get(non_terminal);
+    }
 
+
+
+     // 归约动作，这时候返回产生式的长度，然后栈中弹出这一串，并把产生式的左部分压栈
+//    private SetOfItems CLOSURE(SetOfItems I){
+//        int size = 0;
+//        while(true){
+//            for(Item item: I.items) {
+//                //String
+//                for (Production production : productions) {
+//                    if (production.getLeft().equals("B")){
+//                        int length = item.right.length - item.dot + 1;
+//                        // dot_right里存放着 [A -> α.Bβ, b] 中的 βb 部分
+//                        String[] dot_right = new String[length];
+//                        System.arraycopy(item.right, item.dot, dot_right, 0, length);
+//                        for (String b : getFirst(dot_right)) {
+//                            Item newItem = new Item(production, b);
+//                            I.insert(newItem);
+//                        }
+//                    }
+//                }
+//            }
+//            // until 不能向I中加入更多的项
+//            if(I.size > size){
+//                size = I.size;
+//            }else
+//                return I;
+//        }
+//    }
+
+    // 某个符号能否推导出ε
+    private boolean ifCanEmpty(String symbol){
+        if(this.terminals.contains(symbol))
+            return false;
+        if(this.non_terminals.contains(symbol)){
+            ArrayList<Production> productions = classified_production.get(symbol);
+            for(int i=0; i<productions.size(); i++){
+                if(productions.get(i).getRight().length == 0)
+                    return true;
+            }
+            return false;
+        }
+        System.out.println(symbol + "  该符号不存在！");
+        return false;
+    }
+
+    // 求出所有符号的first集
+    private void makeFirst(){
+        //Object[] terminals = this.terminals.keySet().toArray();
+        //Object[] non_terminals = this.non_terminals.keySet().toArray();
+        ArrayList<String> firstSet;
+        // 所有终结符的first集
+        for(int i=0; i<this.terminals.size(); i++){
+            String terminal = terminals.get(i);
+            firstSet = new ArrayList<>(1);
+            firstSet.add(terminal);
+            this.firstSet.put(terminal, firstSet);
+        }
+        // 非终结符的first集
+        int count = 0;
+        int prevcount = 0;
+        int flag = 0;
+        while(flag++ < 10) {
+            for (int i = 0; i < this.non_terminals.size(); i++) {
+                String non_terminal = this.non_terminals.get(i);
+                firstSet = new ArrayList<>(MAX_LENGTH);
+                ArrayList<Production> productions = getProductionBySymbol(non_terminal);
+                int size = productions.size();
+                int cnt;
+                for (int j = 0; j < size; j++) {
+                    cnt = -1;
+                    String[] right = productions.get(j).getRight();
+                    // 首先查right的第一个符号，如果这个符号不能推出ε，那么就不往后延伸
+                    for (int k = 0; k < right.length; k++) {
+                        ArrayList<String> tmpFirstSet = this.getFirst(right[k]);
+                        if(tmpFirstSet == null)
+                            continue;
+                        for (int m = 0; m < tmpFirstSet.size(); m++) {
+                            if (!firstSet.contains(tmpFirstSet.get(m))) {
+                                firstSet.add(tmpFirstSet.get(m));
+                                count++;
+                            }
+                        }
+                        //firstSet.addAll(tmpFirstSet);
+                        if (!this.ifCanEmpty(right[k]))
+                            break;
+                        cnt = k;
+                    }
+                    if (cnt == right.length - 1 && !firstSet.contains("")) {
+                        firstSet.add("");
+                        count++;
+                    }
+                }
+                this.firstSet.put(non_terminal, firstSet);
+            }
+            // until没有新的加进去，结束
+            if(count == prevcount)
+                break;
+            prevcount = count;
+        }
+    }
+
+    /*
+     * 查找某个符号的first集
+     */
+    private ArrayList<String> getFirst(String symbol){
+        if(!firstSet.containsKey(symbol)){
+            //System.out.println("该符号不存在！");
+            return null;
+        }
+        return firstSet.get(symbol);
     }
 
     // LR(1)文法
@@ -146,11 +265,62 @@ public class Parse {
         }
     }
 
+    private void output() throws FileNotFoundException {
+        File file = new File("F:\\output.txt");
+        PrintWriter pw = new PrintWriter(file);
+        pw.println("*************终结符**************");
+        pw.flush();
+        for(int i=0; i<this.terminals.size(); i++){
+            //System.out.println(this.terminals.get(i));
+            pw.println(i + " " + this.terminals.get(i) + "\t");
+            pw.flush();
+        }
+        pw.println();
+        pw.println("*************非终结符**************");
+        pw.flush();
+        for(int i=0; i<this.non_terminals.size(); i++){
+            pw.println(i + " " + this.non_terminals.get(i) + "\t");
+            pw.flush();
+        }
+        pw.println();
+        pw.println("*************First集合**************");
+        pw.println("*************终结符**************");
+        pw.flush();
+        for(int i=0; i<this.terminals.size(); i++){
+            pw.print(i + " " + "符号：\t" + this.terminals.get(i));
+            ArrayList<String> firstSet = this.firstSet.get(terminals.get(i));
+            for(int j=0; j<firstSet.size(); j++){
+                pw.println("\t" + firstSet.get(j) + "\t");
+                pw.flush();
+            }
+        }
+        pw.println("*************非终结符**************");
+        for(int i=0; i<this.non_terminals.size(); i++){
+            pw.print(i + " " + "符号：\t" + this.non_terminals.get(i));
+            ArrayList<String> firstSet = this.firstSet.get(non_terminals.get(i));
+            for(int j=0; j<firstSet.size(); j++){
+                pw.print("\t"+firstSet.get(j) + "\t");
+                pw.flush();
+            }
+            pw.println();
+        }
+        pw.flush();
+        pw.close();
+
+    }
+
 
 	public static void main(String[] args) {
         Parse parse = new Parse();
-        String filename = "F:\\123.c";
-        parse.parser(filename);
+        try {
+            parse.readIn();
+            parse.classify_production();
+            //System.out.println();
+            parse.makeFirst();
+            parse.output();
+        }catch (IOException e){
+            e.printStackTrace();
+        }
 
 	}
 
